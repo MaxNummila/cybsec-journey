f7154c112ebf3f42149859c4f4e57b4396ccf239
MNummila <maxnummila@gmail.com>
2026-09-04 22:57:15 +0300

    Added multiple host scanning, next is host discovery

--- NetworkingProjects/NetworkScanner/NetworkScanner.py
+++ NetworkingProjects/NetworkScanner/NetworkScanner.py
@@ -1,26 +1,28 @@
 import ipaddress
 import socket
-import errno
 
 inputIP = input("Please enter an IP Address: ")
-network = ipaddress.ip_network(str(inputIP), strict=False)
+network = ipaddress.ip_network(str(inputIP + '/24'), strict=False)
 ports = [22, 80, 43, 8080]
 
-for port in ports:
-    socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_obj.settimeout(2)
+for host in network.hosts():
+    if ipaddress.ip_address(host) in network:
+        for port in ports:
+            socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket_obj.settimeout(2)
 
-    try:
-        socket_obj.connect((str(inputIP), port))
-        print(f"{inputIP}:{port} is open")
-    except socket.timeout:
-        print(f"{inputIP}:{port} timed out")
+            try:
+                socket_obj.connect((str(host), port))
+                print(f"{host}:{port} is open")
+            except socket.timeout:
+                print(f"{host}:{port} timed out")
 
-    except ConnectionRefusedError:
-        print(f"{inputIP}:{port} connection refused")
+            except ConnectionRefusedError:
+                print(f"{host}:{port} connection refused")
 
-    except OSError as e:
-        print(f"{inputIP}:{port} failed: {e}")
+            except OSError as e:
+                print(f"{host}:{port} failed: {e}")
 
-    finally:
-        socket_obj.close()
+            finally:
+                socket_obj.close()
+
