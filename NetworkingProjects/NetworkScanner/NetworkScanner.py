9aeaf7a2c85d19f7a1c6ea63ab652aecc1df2c67
MNummila <maxnummila@gmail.com>
2026-09-04 17:11:08 +0300

    An updated version of the scanner using connect instead of connect_ex

--- NetworkingProjects/NetworkScanner/NetworkScanner.py
+++ NetworkingProjects/NetworkScanner/NetworkScanner.py
@@ -3,20 +3,24 @@ import socket
 import errno
 
 inputIP = input("Please enter an IP Address: ")
-
-
 network = ipaddress.ip_network(str(inputIP), strict=False)
-
 ports = [22, 80, 43, 8080]
 
 for port in ports:
     socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_obj.settimeout(1)
+    socket_obj.settimeout(2)
 
-    result = (socket_obj.connect_ex((inputIP, port)))
-    if result == 0:
+    try:
+        socket_obj.connect((str(inputIP), port))
         print(f"{inputIP}:{port} is open")
-    else:
-        print(result)
-        print(errno.errorcode.get(result))
-    socket_obj.close()
+    except socket.timeout:
+        print(f"{inputIP}:{port} timed out")
+
+    except ConnectionRefusedError:
+        print(f"{inputIP}:{port} connection refused")
+
+    except OSError as e:
+        print(f"{inputIP}:{port} failed: {e}")
+
+    finally:
+        socket_obj.close()
